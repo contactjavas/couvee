519f675743c262f57e1cd17c7807f006f9271f3c
Bagus <25834188+contactjavas@users.noreply.github.com>
2020-07-13 00:52:08 +0700

    merchandise screen: adjustment to the big "Redeem Now" area at the bottom

--- lib/widgets/merchandise/merchandise_details/merchandise_bottom.dart
+++ lib/widgets/merchandise/merchandise_details/merchandise_bottom.dart
@@ -9,74 +9,85 @@ class MerchandiseBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        color: CompanyColors.brown,
-        borderRadius: BorderRadius.circular(15.0),
+      padding: EdgeInsets.only(
+        top: 7.0,
       ),
-      padding: EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "$ptsSum pts",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                ),
-              ),
-              SizedBox(
-                height: 7.0,
-              ),
-              Text(
-                "3 items",
-                style: TextStyle(
-                  fontSize: 12.0,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-          FlatButton(
-            onPressed: () {
-              showModalBottomSheet(
-                  context: context,
-                  backgroundColor: Colors.white,
-                  isDismissible: true,
-                  isScrollControlled: true,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(
-                        35.0,
-                      ),
-                      topRight: Radius.circular(
-                        35.0,
-                      ),
-                    ),
-                  ),
-                  builder: (BuildContext context) {
-                    return QrBottomModal();
-                  });
-            },
-            child: Row(
+      color: Colors.white,
+      child: Container(
+        decoration: BoxDecoration(
+          color: CompanyColors.brown,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        padding: EdgeInsets.only(
+          top: 16.0,
+          right: 10.0,
+          bottom: 16.0,
+          left: 20.0,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                SizedBox(
-                  height: 14.0,
-                  child: Image.asset("assets/images/coin-solid.png"),
-                ),
-                SizedBox(width: 3.0),
                 Text(
-                  "Redeem now",
+                  "$ptsSum pts",
                   style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 7.0,
+                ),
+                Text(
+                  "3 items",
+                  style: TextStyle(
+                    fontSize: 12.0,
                     color: Colors.white,
                   ),
                 ),
               ],
             ),
-          )
-        ],
+            FlatButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    backgroundColor: Colors.white,
+                    isDismissible: true,
+                    isScrollControlled: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(
+                          35.0,
+                        ),
+                        topRight: Radius.circular(
+                          35.0,
+                        ),
+                      ),
+                    ),
+                    builder: (BuildContext context) {
+                      return QrBottomModal();
+                    });
+              },
+              child: Row(
+                children: <Widget>[
+                  SizedBox(
+                    height: 14.0,
+                    child: Image.asset("assets/images/coin-solid.png"),
+                  ),
+                  SizedBox(width: 3.0),
+                  Text(
+                    "Redeem Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
